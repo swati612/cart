069973d44688a619d62eb62a6752cb0a27a98674
swati612 <surbhigarg30000@gmail.com>
2020-11-25 13:35:29 +0530

    add image and icon to cart

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,22 +1,41 @@
 import React from 'react';
 
 class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            price:999,
+            title:'Phone',
+            quantity:1,
+            img:''
+        }
+    }
     render(){
+        const {price,quantity} = this.state
         return(
             <div className="cart-item">
-            <div className="left-block">
-            <img style={styles.image}/>
+              <div className="left-block">
+              <img style={styles.image}/>
+              </div>
+              <div className="right-block">
+                    <div style={{fontSize:25}}>{this.state.title}</div>
+                    <div style={{color :'#777'}}>Rs {price}</div>
+                    <div style={{color :'#777'}}>{quantity}</div>
+                    <div className="cart-item-actions">
+                     {/* kjhsdkhksd */}
+                            <img alt="increase" 
+                            className="action-icons" 
+                            src="https://www.flaticon.com/svg/static/icons/svg/1828/1828926.svg" />
+                            <img alt="decrease"
+                            className="action-icons"
+                            src="https://www.flaticon.com/svg/static/icons/svg/1828/1828906.svg" />
+                            <img alt="delete" 
+                            className="action-icons"
+                            src="https://www.flaticon.com/svg/static/icons/svg/1214/1214428.svg" />
+                    </div>
+               </div>
             </div>
-            <div className="right-block">
-            <div style={{fontSize:25}}>Phone</div>
-            <div style={{color :'#777'}}>Rs 999</div>
-            <div style={{color :'#777'}}>1</div>
-            <div className="cart-item-actions">
-        {/* kjhsdkhksd */}
-            </div>
-            </div>
-            </div>
-        )
+            )
     }
 }
 
