2dbf5928f1cc6b67bf64b8a70884a0826e4acdca
swati612 <surbhigarg30000@gmail.com>
2020-11-25 12:27:41 +0530

    Add cart item component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
 import React from 'react';
+import CartItem from './CartItem';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+    
+    <CartItem />
     </div>
   );
 }
